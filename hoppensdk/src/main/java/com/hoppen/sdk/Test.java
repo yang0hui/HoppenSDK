21ad44e14ae3ab6d4d05c1bc1d7f38d93b5b9b42
YangJianHui <qq824760751>
2020-05-12 14:51:00 +0800

    测试

---
+++ hoppensdk/src/main/java/com/hoppen/sdk/Test.java
@@ -0,0 +1,12 @@
+package com.hoppen.sdk;
+
+import android.util.Log;
+
+/**
+ * Created by YangJianHui on 2020/5/12.
+ */
+public class Test {
+    public Test(){
+        Log.e("测试","hoppenSdk");
+    }
+}
